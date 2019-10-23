aafa0197d0d63821744ca04fedf647ad3bdfe417
Kevin Harrington <mad.hephaestus@gmail.com>
2019-10-23 08:57:06 -0400

    Update template.ino

--- template.ino
+++ template.ino
@@ -14,44 +14,55 @@
 #include "esp_adc_cal.h"
 
 
-double interruptCounter50Hz = 0;
-double interruptCounter1kHz = 0;
-double sinComp = 0;
-double sinComp50Hz = 0;
-double numSamples = 20;
-double frequencyTarget = 1000;
-double frequencyTarget2 = 50;
-int count = (1.0 / (frequencyTarget / 1000000.0)) / numSamples;
-double increment = (2 * PI) / numSamples;
-double increment50Hz = increment / (frequencyTarget/frequencyTarget2);
+
+/*
+ * Timer stuff. Do not edit!
+ */
 // Pointer to the timer object
 hw_timer_t *timer = NULL;
 portMUX_TYPE DRAM_ATTR timerMux = portMUX_INITIALIZER_UNLOCKED;
 TaskHandle_t complexHandlerTask;
 
+/*
+ * Student will edit samplingFreq
+ */
+uint32_t samplingFreq = 1000; //Hz
 
-void runDDS() {
-	interruptCounter1kHz += increment;
-	if (interruptCounter1kHz >= 2 * PI) {
-		interruptCounter1kHz = 0;
-	}
-	interruptCounter50Hz += increment50Hz;
-	if (interruptCounter50Hz >= 2 * PI) {
-		interruptCounter50Hz = 0;
-	}
-	sinComp = (126.0  * sin(interruptCounter1kHz)) + 128.0 ;
-	sinComp50Hz = (126.0 * sin(interruptCounter50Hz)) + 128.0;
-	analogWrite(26, (uint8_t) ( sinComp));
-	analogWrite(25, (uint8_t) (sinComp50Hz ));
+/*
+ * Here is where you implement your filter
+ */
+void studentCode()
+{
+	int adc = local_adc1_read(0); //reads ADC1:0, which is pin 36, 12-bit conversion
+	static int dac = 0;
+
+	dac = adc >> 4; //the dac takes an 8-bit value, so divide the adc by 16
+
+	analogWrite(25, (uint8_t) dac); //pin 25 is DAC1, but note that it's 8-bit
 }
 
+/*
+ * Dragons below
+ */
 
+int IRAM_ATTR local_adc1_read(int channel) {
+	uint16_t adc_value;
+	SENS.sar_meas_start1.sar1_en_pad = (1 << channel); // only one channel is selected
+	while (SENS.sar_slave_addr1.meas_status != 0)
+		;
+	SENS.sar_meas_start1.meas1_start_sar = 0;
+	SENS.sar_meas_start1.meas1_start_sar = 1;
+	while (SENS.sar_meas_start1.meas1_done_sar == 0)
+		;
+	adc_value = SENS.sar_meas_start1.meas1_data_sar;
+	return adc_value;
+}
 
 void complexHandler(void *param) {
 	while (true) {
 		// Sleep until the ISR gives us something to do, or for 1 second
 		ulTaskNotifyTake(pdFALSE, pdMS_TO_TICKS(1000));
-		runDDS();
+		studentCode();
 	}
 }
 // Place the Interrupt handeler in IRAM section of memory
@@ -71,6 +82,8 @@ void IRAM_ATTR onTimer() {
 }
 
 void setup() {
+	uint32_t count = 1000000 / samplingFreq; //count is in us -- don't edit this
+
 	xTaskCreate(complexHandler, "Handler Task", 8192, NULL, 1,
 			&complexHandlerTask);
 	Serial.begin(115200);
@@ -85,12 +98,27 @@ void setup() {
 			&onTimer, // Interrupt handler passed to timer
 			true); // Rising edge of timer
 	timerAlarmWrite(timer, // The timer object to use
-			count, // count, now now in us
+			count, // count, now in us
 			true); // Reload after finishes, run again and again
 	timerAlarmEnable(timer); // Enable timer
+	analogRead(36);
 
+#define V_REF 1100  // ADC reference voltage
+
+    // Configure ADC
+    adc1_config_width(ADC_WIDTH_12Bit);
+    adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_11db);
+
+    // Calculate ADC characteristics i.e. gain and offset factors
+    esp_adc_cal_characteristics_t characteristics;
+    esp_adc_cal_get_characteristics(V_REF, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, &characteristics);
+    uint32_t voltage=0;
+    // Read ADC and obtain result in mV
+    uint32_t err = esp_adc_cal_get_voltage(ADC_CHANNEL_0, &characteristics,&voltage);
+    printf("%d mV\n",voltage);
 }
 
 void loop() {
+	//Serial.println("Looping");
 	delay(100);
 }
